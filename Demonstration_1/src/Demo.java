bbe16109e22332bf9a2480d7e58ba94589997e21
sushma <sushma@LAPTOP-89UFPBAO>
2020-02-18 15:39:59 +0530

    registration form

---
+++ Demonstration_1/src/Demo.java
@@ -0,0 +1,18 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Demo1 c1;
+		c1= new Demo1();
+		Demo1 c2= new Demo1(7);
+		
+	
+		System.out.println(c1.num1);
+		System.out.println(c2.y);
+	
+		
+
+	}
+
+}
---
+++ Demonstration_1/src/Demo1.java
@@ -0,0 +1,14 @@
+
+public class Demo1 {
+	int num1;
+	int y;
+	public Demo1() {
+		 num1=5;
+	}
+	
+	public Demo1(int n) {
+		 y=n;
+		System.out.println("in constructor");
+
+	}
+}
